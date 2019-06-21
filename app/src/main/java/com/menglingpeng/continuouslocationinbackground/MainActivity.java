5533e2c2510faaf84a3865208ce525b898dd68ec
menglingpeng <menglingpeng@live.com>
2019-06-21 14:37:57 +0800

    feat: add the basic layout of main page

--- app/src/main/java/com/menglingpeng/continuouslocationinbackground/MainActivity.java
+++ app/src/main/java/com/menglingpeng/continuouslocationinbackground/MainActivity.java
@@ -1,13 +1,39 @@
 package com.menglingpeng.continuouslocationinbackground;
 
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private FloatingActionButton floatingActionButton;
+    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.main_fab);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_cdl);
+        toolbar = (Toolbar) findViewById(R.id.main_tb);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_grey_600_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
