6b380979ad7c14bd810f57039c7eceb4c30dc491
menglingpeng <menglingpeng@live.com>
2019-06-22 12:17:07 +0800

    feat: add the function of init the basic map

--- app/src/main/java/com/menglingpeng/continuouslocationinbackground/MainActivity.java
+++ app/src/main/java/com/menglingpeng/continuouslocationinbackground/MainActivity.java
@@ -7,11 +7,18 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private FloatingActionButton floatingActionButton;
     private CoordinatorLayout coordinatorLayout;
+    private MapView mapView;
+    private AMap aMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         floatingActionButton = (FloatingActionButton) findViewById(R.id.main_fab);
         coordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_cdl);
         toolbar = (Toolbar) findViewById(R.id.main_tb);
+        mapView = (MapView) findViewById(R.id.main_mv);
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_grey_600_24dp);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -35,5 +43,45 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        //创建地图
+        mapView.onCreate(savedInstanceState);
+        initMap();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mapView.onResume ()，重新绘制加载地图
+        mapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mapView.onPause ()，暂停地图的绘制
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mapView.onDestroy()，销毁地图
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 初始化AMap对象
+     */
+    private void initMap() {
+        if(aMap == null){
+            aMap = mapView.getMap();
+        }
+
     }
 }
