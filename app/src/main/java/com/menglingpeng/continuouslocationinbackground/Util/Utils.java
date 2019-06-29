79f69819c3695eb05cb5179f3402a514115dbcf1
menglingpeng <menglingpeng@live.com>
2019-06-29 11:04:08 +0800

    feat: add the function of get loaction string

--- app/src/main/java/com/menglingpeng/continuouslocationinbackground/Util/Utils.java
+++ app/src/main/java/com/menglingpeng/continuouslocationinbackground/Util/Utils.java
@@ -10,8 +10,13 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Build;
+import android.text.TextUtils;
 
+import com.amap.api.location.AMapLocation;
+
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class Utils {
 
@@ -21,6 +26,78 @@ public class Utils {
         Notification.Builder builder = new Notification.Builder(context);
         builder.setContentText("service");
         return builder.getNotification();
+    }
+
+    /**
+     * 根据定位结果返回定位信息的字符串
+     *
+     * @param location
+     * @return
+     */
+    public synchronized static String getLocationStr(AMapLocation location) {
+        if (null == location) {
+            return null;
+        }
+        StringBuffer sb = new StringBuffer();
+        //errCode等于0代表定位成功，其他的为定位失败，具体的可以参照官网定位错误码说明
+        if (location.getErrorCode() == 0) {
+            sb.append("定位成功" + "\n");
+            sb.append("定位类型: " + location.getLocationType() + "\n");
+            sb.append("经    度    : " + location.getLongitude() + "\n");
+            sb.append("纬    度    : " + location.getLatitude() + "\n");
+            sb.append("精    度    : " + location.getAccuracy() + "米" + "\n");
+            sb.append("提供者    : " + location.getProvider() + "\n");
+
+            sb.append("海    拔    : " + location.getAltitude() + "米" + "\n");
+            sb.append("速    度    : " + location.getSpeed() + "米/秒" + "\n");
+            sb.append("角    度    : " + location.getBearing() + "\n");
+            if (location.getProvider().equalsIgnoreCase(
+                    android.location.LocationManager.GPS_PROVIDER)) {
+                // 以下信息只有提供者是GPS时才会有
+                // 获取当前提供定位服务的卫星个数
+                sb.append("星    数    : "
+                        + location.getSatellites() + "\n");
+            }
+
+            //逆地理信息
+            sb.append("国    家    : " + location.getCountry() + "\n");
+            sb.append("省            : " + location.getProvince() + "\n");
+            sb.append("市            : " + location.getCity() + "\n");
+            sb.append("城市编码 : " + location.getCityCode() + "\n");
+            sb.append("区            : " + location.getDistrict() + "\n");
+            sb.append("区域 码   : " + location.getAdCode() + "\n");
+            sb.append("地    址    : " + location.getAddress() + "\n");
+            sb.append("兴趣点    : " + location.getPoiName() + "\n");
+            //定位完成的时间
+            sb.append("定位时间: " + formatUTC(location.getTime(), "yyyy-MM-dd HH:mm:ss") + "\n");
+
+        } else {
+            //定位失败
+            sb.append("定位失败" + "\n");
+            sb.append("错误码:" + location.getErrorCode() + "\n");
+            sb.append("错误信息:" + location.getErrorInfo() + "\n");
+            sb.append("错误描述:" + location.getLocationDetail() + "\n");
+        }
+        //定位之后的回调时间
+        sb.append("回调时间: " + formatUTC(System.currentTimeMillis(), "yyyy-MM-dd HH:mm:ss") + "\n");
+        return sb.toString();
+    }
+
+    private static SimpleDateFormat sdf = null;
+
+    public synchronized static String formatUTC(long l, String strPattern) {
+        if (TextUtils.isEmpty(strPattern)) {
+            strPattern = "yyyy-MM-dd HH:mm:ss";
+        }
+        if (sdf == null) {
+            try {
+                sdf = new SimpleDateFormat(strPattern, Locale.CHINA);
+            } catch (Throwable e) {
+            }
+        } else {
+            sdf.applyPattern(strPattern);
+        }
+        return sdf == null ? "NULL" : sdf.format(l);
     }
 
     public static Intent getExplicitIntent(Context context, Intent implicitIntent) {
